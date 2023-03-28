0a5e0118c7be6ad7ae38c30299e2db5de43b59e1
ambitious_hustler <deepaks9631342845@gmail.com>
2023-03-29 02:01:08 +0530

    Create searchinbst.cpp

---
+++ searchinbst.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h> 
+/*
+    Following is the Binary Tree node structure:
+
+    class BinaryTreeNode {
+    public : 
+        T data;
+        BinaryTreeNode<T> *left;
+        BinaryTreeNode<T> *right;
+
+        BinaryTreeNode(T data) {
+            this -> data = data;
+            left = NULL;
+            right = NULL;
+        }
+    };
+*/
+
+bool searchInBST(BinaryTreeNode<int> *root, int x) {
+    // Write your code here.
+    if (root == NULL) {
+    return false;}
+    if (root->data == x) {
+    return true;}
+    if (x > root->data) {
+    return searchInBST(root->right, x);
+    } else {
+        return searchInBST(root->left, x);
+    }
+    //return false;
+}
